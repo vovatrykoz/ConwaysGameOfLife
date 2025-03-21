4f47701a8950c3a15178882690954b7760365b2b
vovatrykoz <vovatrykoz@gmail.com>
2025-03-21 14:14:50 +0100

    add simple unit tests

--- tests/Conway.Tests/UnitTest1.fs
+++ tests/Conway.Tests/UnitTest1.fs
@@ -1,11 +1,20 @@
 module Conway.Tests
 
+open Conway.Core
 open NUnit.Framework
 
-[<SetUp>]
-let Setup () =
-    ()
+module ``Cell Tests`` =
 
-[<Test>]
-let Test1 () =
-    Assert.Pass()
+    [<Test>]
+    let ``Can create a living cell using the dedicated method on the Cell type`` () =
+        let expected = { Status = Alive }
+        let actual = Cell.createLivingCell
+
+        Assert.That(actual, Is.EqualTo expected)
+
+    [<Test>]
+    let ``Can create a dead cell using the dedicated method on the Cell type`` () =
+        let expected = { Status = Dead }
+        let actual = Cell.createDeadCell
+
+        Assert.That(actual, Is.EqualTo expected)
