7139bf8d4039f8f8329352c469147b4600c20bb4
vovatrykoz <vovatrykoz@gmail.com>
2025-04-15 17:22:41 +0200

    add an avalonia project

---
+++ src/Conway.App.Desktop/Program.fs
@@ -0,0 +1,67 @@
+﻿namespace Conway.App.Desktop
+
+open Avalonia
+open Avalonia.Controls.ApplicationLifetimes
+open Avalonia.Themes.Fluent
+open Avalonia.FuncUI.Hosts
+open Avalonia.Controls
+open Avalonia.FuncUI
+open Avalonia.FuncUI.DSL
+open Avalonia.Layout
+
+module Main =
+
+    let view () =
+        Component(fun ctx ->
+            let state = ctx.useState 0
+
+            DockPanel.create [
+                DockPanel.children [
+                    Button.create [
+                        Button.dock Dock.Bottom
+                        Button.onClick (fun _ -> state.Set(state.Current - 1))
+                        Button.content "-"
+                        Button.horizontalAlignment HorizontalAlignment.Stretch
+                        Button.horizontalContentAlignment HorizontalAlignment.Center
+                    ]
+                    Button.create [
+                        Button.dock Dock.Bottom
+                        Button.onClick (fun _ -> state.Set(state.Current + 1))
+                        Button.content "+"
+                        Button.horizontalAlignment HorizontalAlignment.Stretch
+                        Button.horizontalContentAlignment HorizontalAlignment.Center
+                    ]
+                    TextBlock.create [
+                        TextBlock.dock Dock.Top
+                        TextBlock.fontSize 48.0
+                        TextBlock.verticalAlignment VerticalAlignment.Center
+                        TextBlock.horizontalAlignment HorizontalAlignment.Center
+                        TextBlock.text (string state.Current)
+                    ]
+                ]
+            ])
+
+type MainWindow() =
+    inherit HostWindow()
+
+    do
+        base.Title <- "Conway's Game of Life"
+        base.Content <- Main.view ()
+
+type App() =
+    inherit Application()
+
+    override this.Initialize() =
+        this.Styles.Load "avares://Conway.App.Desktop/Assets/Styles/Night.axaml"
+        this.RequestedThemeVariant <- Styling.ThemeVariant.Light
+
+    override this.OnFrameworkInitializationCompleted() =
+        match this.ApplicationLifetime with
+        | :? IClassicDesktopStyleApplicationLifetime as desktopLifetime -> desktopLifetime.MainWindow <- MainWindow()
+        | _ -> ()
+
+module Program =
+
+    [<EntryPoint>]
+    let main (args: string[]) =
+        AppBuilder.Configure<App>().UsePlatformDetect().UseSkia().StartWithClassicDesktopLifetime(args)
