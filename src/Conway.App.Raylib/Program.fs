9fa693c82c84c088dcdf01afd7eda3d4e3924ce9
vovatrykoz <vovatrykoz@gmail.com>
2025-04-17 15:26:38 +0200

    can start and pause the game

--- src/Conway.App.Raylib/Program.fs
+++ src/Conway.App.Raylib/Program.fs
@@ -1,36 +1,58 @@
 ﻿open Conway.Core
 open Conway.App.Raylib
 open Raylib_cs
+open System.Threading
 
 let startingState = Preset.presetOne |> ConwayGrid.initFromPreset
+
+let raylibTrue expr = expr |> Convert.CBoolToFsBool
 
 let game = new Game(startingState)
 
 Display.init ()
 
+let mutex = new Mutex()
+
 let mutable gameRunningState = Infinite
 
 let rec gameUpdateLoop state =
-
     async {
         do! Async.Sleep 500
 
-        match gameRunningState with
-        | Infinite -> game.runOneStep ()
-        | Limited x when x > 1 ->
-            game.runOneStep ()
-            gameRunningState <- Limited(x - 1)
-        | Limited _ ->
-            game.runOneStep ()
-            gameRunningState <- Paused
-        | Paused -> ()
+        try
+            mutex.WaitOne() |> ignore
+
+            match gameRunningState with
+            | Infinite -> game.runOneStep ()
+            | Limited x when x > 1 ->
+                game.runOneStep ()
+                gameRunningState <- Limited(x - 1)
+            | Limited _ ->
+                game.runOneStep ()
+                gameRunningState <- Paused
+            | Paused -> ()
+        finally
+            mutex.ReleaseMutex()
 
         return! gameUpdateLoop state
     }
 
+let readUserInput () =
+    if raylibTrue (Raylib.IsKeyPressed KeyboardKey.Space) then
+        try
+            mutex.WaitOne() |> ignore
+
+            gameRunningState <-
+                match gameRunningState with
+                | Paused -> Infinite
+                | _ -> Paused
+        finally
+            mutex.ReleaseMutex()
+
 gameRunningState |> gameUpdateLoop |> Async.Start
 
-while not (Raylib.WindowShouldClose() |> Convert.CBoolToFsBool) do
+while not (raylibTrue (Raylib.WindowShouldClose())) do
+    readUserInput ()
     Display.render game.State.Board gameRunningState
 
 Display.close ()
