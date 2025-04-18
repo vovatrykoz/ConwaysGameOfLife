053e3083d07c51facdc4f9d29f07429a903ed36e
vovatrykoz <vovatrykoz@gmail.com>
2025-04-18 18:35:18 +0200

    create a simple stack

---
+++ src/Conway.Core/Stack.fs
@@ -0,0 +1,38 @@
+namespace Conway.Core
+
+type Stack<'T> = private {
+    Contents: list<'T>
+} with
+
+    [<CompiledName("Create")>]
+    static member create: Stack<'T> = { Contents = List.empty }
+
+    [<CompiledName("Push")>]
+    static member push value stack = {
+        stack with
+            Contents = value :: stack.Contents
+    }
+
+    [<CompiledName("Pop")>]
+    static member pop stack =
+        match stack.Contents with
+        | x :: xs -> x, { stack with Contents = xs }
+        | [] -> invalidOp "The stack was empty"
+
+    [<CompiledName("TryPop")>]
+    static member tryPop stack =
+        match stack.Contents with
+        | x :: xs -> Some x, { stack with Contents = xs }
+        | [] -> None, stack
+
+    [<CompiledName("Peek")>]
+    static member peek stack =
+        match stack.Contents with
+        | x :: _ -> x
+        | [] -> invalidOp "The stack was empty"
+
+    [<CompiledName("TryPeek")>]
+    static member tryPeek stack =
+        match stack.Contents with
+        | x :: _ -> Some x
+        | [] -> None
