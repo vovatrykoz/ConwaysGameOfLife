c45326f45b7f3e952f5a5d28e3871ef486096ac5
vovatrykoz <vovatrykoz@gmail.com>
2025-04-22 17:49:59 +0200

    use dark background

--- src/Conway.App.Raylib/Draw.fs
+++ src/Conway.App.Raylib/Draw.fs
@@ -3,11 +3,19 @@ namespace Conway.App.Raylib
 open Raylib_cs
 
 module Draw =
-    let border x y width height color =
-        Raylib.DrawRectangleLines(x * width, y * height, width, height, color)
+    let private buttonBorderRectangle x y size =
+        Rectangle(float32 (x - 2), float32 (y - 2), float32 (size + 4), float32 (size + 4))
+
+    let inline private calculateBorderSize buttonSize = float32 buttonSize / 12.5f
+
+    let border x y width height thickness color =
+        let rectangle =
+            Rectangle(float32 x * float32 width, float32 y * float32 height, float32 width, float32 height)
+
+        Raylib.DrawRectangleLinesEx(rectangle, thickness, color)
 
     let cell x y width height color =
-        border x y width height Color.Black
+        Raylib.DrawRectangle(x * width, y * height, width, height, Color.Black)
         Raylib.DrawRectangle(x * width + 1, y * height + 1, width - 2, height - 2, color)
 
     let livingCell x y width height = cell x y width height Color.Red
@@ -25,13 +33,8 @@ module Draw =
         | false -> ()
         | true ->
             Raylib.DrawRectangleLinesEx(
-                new Rectangle(
-                    float32 (button.X - 2),
-                    float32 (button.Y - 2),
-                    float32 (button.Size + 4),
-                    float32 (button.Size + 4)
-                ),
-                2.0f,
+                buttonBorderRectangle button.X button.Y button.Size,
+                calculateBorderSize button.Size,
                 Color.Black
             )
 
