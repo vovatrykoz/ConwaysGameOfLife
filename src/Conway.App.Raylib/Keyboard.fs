936fa9fd826ec4d3496151b254d2907255f3646b
vovatrykoz <vovatrykoz@gmail.com>
2025-04-24 21:27:27 +0200

    add retyping to input reading functions

--- src/Conway.App.Raylib/Keyboard.fs
+++ src/Conway.App.Raylib/Keyboard.fs
@@ -7,19 +7,3 @@ module Keyboard =
     let readKeyDown key = raylibTrue (Raylib.IsKeyDown key)
 
     let readKeyPress key = raylibTrue (Raylib.IsKeyPressed key)
-
-    let readSpacePress callback =
-        if raylibTrue (Raylib.IsKeyPressed KeyboardKey.Space) then
-            callback ()
-
-    let readRightArrowKey callback =
-        if raylibTrue (Raylib.IsKeyPressed KeyboardKey.Right) then
-            callback ()
-
-    let readLeftArrowKey callback =
-        if raylibTrue (Raylib.IsKeyPressed KeyboardKey.Left) then
-            callback ()
-
-    let readKeyPresses keyCallbackPairs =
-        keyCallbackPairs
-        |> Seq.iter (fun (keyPressFunc, callback) -> keyPressFunc callback)
--- src/Conway.App.Raylib/RaylibTypeConverter.fs
+++ src/Conway.App.Raylib/RaylibTypeConverter.fs
@@ -2,11 +2,19 @@ namespace Conway.App.Raylib
 
 open Raylib_cs
 
+// Warning FS0042: This construct is deprecated: it is only for use in the F# library
+#nowarn "42"
+
+module private UnsafeUtils =
+
+    // This is generally bad and should be avoided
+    // As seen from the warning, should only really be used in the core F# library
+    // This IL instruction essentially does a "reinterpret_cast", except with values instead of pointers
+    // This idea was taken from the "F# for Performance-Critical Code" talk by Matthew Crews
+    let inline retype<'T, 'U> (x: 'T) : 'U = (# "" x: 'U #)
+
 module Convert =
-    let CBoolToFsBool (cbool: CBool) =
-        match sbyte cbool with
-        | 0y -> false
-        | _ -> true
+    let inline CBoolToFsBool (cbool: CBool) = UnsafeUtils.retype<CBool, bool> cbool
 
 module Aliases =
-    let raylibTrue expr = expr |> Convert.CBoolToFsBool
+    let inline raylibTrue expr = expr |> Convert.CBoolToFsBool
