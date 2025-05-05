a35f9d310213e9bc7546d9671615b116547fd5a0
vovatrykoz <vovatrykoz@gmail.com>
2025-05-05 11:48:44 +0200

    move the GameArea module into a separate file

--- src/Conway.App.Raylib/Canvas.fs
+++ src/Conway.App.Raylib/Canvas.fs
@@ -4,51 +4,6 @@ open Conway.Core
 open Raylib_cs
 open System.Numerics
 
-module private CanvasArea =
-    let makeAlive row col (game: Game) =
-        game.State.Board[row, col] <- 1<CellStatus>
-
-        // erase the history since the player has altered the board
-        game.ClearHistory()
-
-    let makeDead row col (game: Game) =
-        game.State.Board[row, col] <- 0<CellStatus>
-
-        // erase the history since the player has altered the board
-        game.ClearHistory()
-
-    let IsPressedWithShift
-        (startX: float32)
-        (startY: float32)
-        (endX: float32)
-        (endY: float32)
-        (mouseButton: MouseButton)
-        =
-        if
-            (Keyboard.keyIsDown KeyboardKey.LeftShift
-             || Keyboard.keyIsDown KeyboardKey.RightShift)
-            && Mouse.buttonIsPressed mouseButton
-        then
-            let mousePos = Mouse.getPosition ()
-
-            if
-                mousePos.X >= startX
-                && mousePos.X <= endX
-                && mousePos.Y >= startY
-                && mousePos.Y <= endY
-            then
-                true
-            else
-                false
-        else
-            false
-
-    let IsLeftPressedWithShift (startX: float32) (startY: float32) (endX: float32) (endY: float32) =
-        IsPressedWithShift startX startY endX endY MouseButton.Left
-
-    let IsRightPressedWithShift (startX: float32) (startY: float32) (endX: float32) (endY: float32) =
-        IsPressedWithShift startX startY endX endY MouseButton.Right
-
 type Canvas
     (
         x: float32,
@@ -152,8 +107,8 @@ type Canvas
                 let endX = min (min (startX + cellSize) endBorderX) trueEndX
                 let endY = min (min (startY + cellSize) endBorderY) trueEndY
 
-                if CanvasArea.IsLeftPressedWithShift startX startY endX endY then
-                    CanvasArea.makeAlive row col this.Game
+                if GameArea.IsLeftPressedWithShift startX startY endX endY then
+                    GameArea.makeAlive row col this.Game
 
-                if CanvasArea.IsRightPressedWithShift startX startY endX endY then
-                    CanvasArea.makeDead row col this.Game
+                if GameArea.IsRightPressedWithShift startX startY endX endY then
+                    GameArea.makeDead row col this.Game
---
+++ src/Conway.App.Raylib/GameArea.fs
@@ -0,0 +1,49 @@
+namespace Conway.App.Raylib
+
+open Conway.Core
+open Raylib_cs
+
+module internal GameArea =
+    let makeAlive row col (game: Game) =
+        game.State.Board[row, col] <- 1<CellStatus>
+
+        // erase the history since the player has altered the board
+        game.ClearHistory()
+
+    let makeDead row col (game: Game) =
+        game.State.Board[row, col] <- 0<CellStatus>
+
+        // erase the history since the player has altered the board
+        game.ClearHistory()
+
+    let IsPressedWithShift
+        (startX: float32)
+        (startY: float32)
+        (endX: float32)
+        (endY: float32)
+        (mouseButton: MouseButton)
+        =
+        if
+            (Keyboard.keyIsDown KeyboardKey.LeftShift
+             || Keyboard.keyIsDown KeyboardKey.RightShift)
+            && Mouse.buttonIsPressed mouseButton
+        then
+            let mousePos = Mouse.getPosition ()
+
+            if
+                mousePos.X >= startX
+                && mousePos.X <= endX
+                && mousePos.Y >= startY
+                && mousePos.Y <= endY
+            then
+                true
+            else
+                false
+        else
+            false
+
+    let IsLeftPressedWithShift (startX: float32) (startY: float32) (endX: float32) (endY: float32) =
+        IsPressedWithShift startX startY endX endY MouseButton.Left
+
+    let IsRightPressedWithShift (startX: float32) (startY: float32) (endX: float32) (endY: float32) =
+        IsPressedWithShift startX startY endX endY MouseButton.Right
