bba2c585e38ed663a414b7428fdd96ad795828a8
vovatrykoz <vovatrykoz@gmail.com>
2025-05-17 13:35:09 +0200

    can decode a grid

--- src/Conway.Encoding/ConwayByteDecoder.fs
+++ src/Conway.Encoding/ConwayByteDecoder.fs
@@ -5,7 +5,9 @@ open System
 open System.Collections.Generic
 
 type ConwayByteDecoder() =
-    static member val NumberOfBytesInInt32 = 4 with get
+    static member val NumberOfBytesInInt32 = sizeof<int> with get
+
+    static member val BitsInByte = 8 with get
 
     static member val RowValueFirstByteIndex = 0 with get
 
@@ -13,20 +15,38 @@ type ConwayByteDecoder() =
 
     static member val GridFirstByteIndex = 8 with get
 
-    member _.DecodeDimensions(bytes: byte array) =
+    member _.DecodeDimensions(bytes: ReadOnlySpan<byte>) =
         let rowSlice =
-            ReadOnlySpan(bytes).Slice(ConwayByteDecoder.RowValueFirstByteIndex, ConwayByteDecoder.NumberOfBytesInInt32)
+            bytes.Slice(ConwayByteDecoder.RowValueFirstByteIndex, ConwayByteDecoder.NumberOfBytesInInt32)
 
         let colSlice =
-            ReadOnlySpan(bytes)
-                .Slice(ConwayByteDecoder.ColumnValueFirstByteIndex, ConwayByteDecoder.NumberOfBytesInInt32)
+            bytes.Slice(ConwayByteDecoder.ColumnValueFirstByteIndex, ConwayByteDecoder.NumberOfBytesInInt32)
 
         let rows = BitConverter.ToInt32 rowSlice
         let cols = BitConverter.ToInt32 colSlice
 
         rows, cols
 
-    member _.DecodeGrid(bytes: byte array) = failwith "Not implemented"
+    member this.DecodeGrid(bytes: ReadOnlySpan<byte>) =
+        let rows, cols = this.DecodeDimensions bytes
+        let totalCells = rows * cols
+        let totalBytes = totalCells / ConwayByteDecoder.BitsInByte
+        let cellArray = Array2D.create rows cols 0<CellStatus>
+
+        bytes.Slice(ConwayByteDecoder.GridFirstByteIndex, totalBytes).ToArray()
+        |> Array.map (fun b -> BitVector8.createFromByte b)
+        |> Array.iteri (fun i b ->
+            for j in 0..7 do
+                let actualIndex = i * ConwayByteDecoder.BitsInByte + j
+                let row = actualIndex / cols
+                let col = actualIndex % cols
+
+                match b.ReadBitAt j with
+                | true -> cellArray.[row, col] <- 1<CellStatus>
+                | false -> cellArray.[row, col] <- 0<CellStatus>)
+
+        let initializer i j = cellArray.[i, j]
+        ConwayGrid.init rows cols initializer
 
     interface IConwayByteDecoder with
         member this.Decode(bytes: byte array) = failwith "Not implemented"
--- tests/Conway.Tests/ConwayByteDecoderTests.fs
+++ tests/Conway.Tests/ConwayByteDecoderTests.fs
@@ -25,3 +25,25 @@ module ``Conway Byte Decoder Tests`` =
         let actualDimensions = decoder.DecodeDimensions encoding
 
         Assert.That(actualDimensions, Is.EqualTo expectedDimensions)
+
+    [<Test>]
+    let ``Can correctly decode a living grid`` () =
+        let encoding = [|
+            0b0000_0100uy // 4 rows (32-bit int)
+            0b0000_0000uy // ...
+            0b0000_0000uy // ...
+            0b0000_0000uy // ...
+            0b0000_0100uy // 4 cols (32-bit int)
+            0b0000_0000uy // ...
+            0b0000_0000uy // ...
+            0b0000_0000uy // ...
+            0b1111_1111uy // 4x4 grid with all cells living
+            0b1111_1111uy // ...
+        |]
+
+        let expectedGrid = ConwayGrid.createLiving 4 4
+
+        let encoder = new ConwayByteDecoder()
+        let actualGrid = encoder.DecodeGrid encoding
+
+        CollectionAssert.AreEqual(expectedGrid.Board, actualGrid.Board)
