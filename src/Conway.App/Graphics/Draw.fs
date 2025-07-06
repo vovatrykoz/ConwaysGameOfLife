5d8b842330514225aa258f3521ab32ef127b1fde
vovatrykoz <vovatrykoz@gmail.com>
2025-07-06 12:35:19 +0200

    Further app refactoring

---
+++ src/Conway.App/Graphics/Draw.fs
@@ -0,0 +1,56 @@
+namespace Conway.App.Graphics
+
+open Conway.App.Controls
+open Raylib_cs
+
+module Draw =
+    open System.Numerics
+
+    let inline private buttonBorderRectangle x y size =
+        Rectangle(float32 (x - 2), float32 (y - 2), float32 (size + 4), float32 (size + 4))
+
+    let inline private calculateBorderSize buttonSize = float32 buttonSize / 12.5f
+
+    let border x y width height thickness color =
+        let rectangle =
+            Rectangle(float32 x * float32 width, float32 y * float32 height, float32 width, float32 height)
+
+        Raylib.DrawRectangleLinesEx(rectangle, thickness, color)
+
+    let cell (x: float32) (y: float32) (width: float32) (height: float32) color =
+        Raylib.DrawRectanglePro(
+            Rectangle(Vector2(x * width, y * height), Vector2(width, height)),
+            Vector2.Zero,
+            0.0f,
+            Color.Black
+        )
+
+        Raylib.DrawRectanglePro(
+            Rectangle(Vector2(x * width + 1.0f, y * height + 1.0f), Vector2(width - 2.0f, height - 2.0f)),
+            Vector2.Zero,
+            0.0f,
+            color
+        )
+
+    let inline livingCell x y width height = cell x y width height Color.Red
+
+    let inline deadCell x y width height = cell x y width height Color.Black
+
+    let button (button: Button) =
+        match button.IsActive with
+        | true -> Raylib.DrawRectangle(button.X, button.Y, button.Size, button.Size, Color.Black)
+        | false -> Raylib.DrawRectangle(button.X, button.Y, button.Size, button.Size, Color.Gray)
+
+        Raylib.DrawText(button.Text, button.X + 5, button.Y + 5, 15, Color.White)
+
+        match button.IsPressed with
+        | false -> ()
+        | true ->
+            Raylib.DrawRectangleLinesEx(
+                buttonBorderRectangle button.X button.Y button.Size,
+                calculateBorderSize button.Size,
+                Color.Black
+            )
+
+    let inline textBox (x: float32) (y: float32) fontSize (text: string) =
+        Raylib.DrawText(text, int x, int y, fontSize, Color.Black)
--- src/Conway.App/Program.fs
+++ src/Conway.App/Program.fs
@@ -1,7 +1,7 @@
-open Conway.App
-open Conway.App.Aliases
 open Conway.App.Controls
+open Conway.App.Graphics
 open Conway.App.File
+open Conway.App.Utils.Aliases
 open Conway.Core
 open Conway.Encoding
 open Raylib_cs
