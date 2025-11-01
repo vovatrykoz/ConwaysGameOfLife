876a319117f369ae3373158f06315653be953f88
vovatrykoz <vovatrykoz@gmail.com>
2025-11-01 17:10:28 +0100

    Fix failing coder/decoder integration tests

--- src/Conway.Encoding/ConwayByteDecoder.fs
+++ src/Conway.Encoding/ConwayByteDecoder.fs
@@ -9,7 +9,7 @@ type ConwayByteDecoder() =
 
     static member val RowValueFirstByteIndex = 0 with get
 
-    static member val ColumnValueFirstByteIndex= 4 with get
+    static member val ColumnValueFirstByteIndex = 4 with get
 
     static member val GenerationCounterFirstByteIndex = 8 with get
 
@@ -35,20 +35,27 @@ type ConwayByteDecoder() =
     member this.DecodeGrid(bytes: ReadOnlySpan<byte>) =
         let rows, cols = this.DecodeDimensions bytes
         let totalCells = rows * cols
-        let totalBytes = totalCells / ConwayByteDecoder.BitsInByte
+        let totalBytes = totalCells / ConwayByteDecoder.BitsInByte + 1
         let cellArray = Array2D.create rows cols ConwayGrid.DeadCell
+
+        let mutable remainingCells = totalCells
 
         bytes.Slice(ConwayByteDecoder.GridFirstByteIndex, totalBytes).ToArray()
         |> Array.map (fun b -> BitVector8.createFromByte b)
         |> Array.iteri (fun i b ->
             for j in 0..7 do
-                let actualIndex = i * ConwayByteDecoder.BitsInByte + j
-                let row = actualIndex / cols
-                let col = actualIndex % cols
+                if remainingCells <= 0 then
+                    ()
+                else
+                    let actualIndex = i * ConwayByteDecoder.BitsInByte + j
+                    let row = actualIndex / cols
+                    let col = actualIndex % cols
 
-                match b.ReadBitAt j with
-                | true -> cellArray.[row, col] <- ConwayGrid.LivingCell
-                | false -> cellArray.[row, col] <- ConwayGrid.DeadCell)
+                    match b.ReadBitAt j with
+                    | true -> cellArray.[row, col] <- ConwayGrid.LivingCell
+                    | false -> cellArray.[row, col] <- ConwayGrid.DeadCell
+
+                    remainingCells <- remainingCells - 1)
 
         let initializer i j = cellArray.[i, j]
         ConwayGrid.init cols rows initializer
