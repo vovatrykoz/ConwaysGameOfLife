719f4da1436de3ccdbf4e341205313ac2d6b5c6f
vovatrykoz <vovatrykoz@gmail.com>
2025-11-30 14:34:12 +0100

    Explicit type annotations for huffman tree

--- src/Conway.Compression/HuffmanTree.fs
+++ src/Conway.Compression/HuffmanTree.fs
@@ -4,13 +4,13 @@ type HuffmanTree<'T> =
     | Leaf of 'T
     | Node of HuffmanNode<'T>
 
-    static member createLeaf<'T> value = Leaf value
+    static member createLeaf<'T>(value: 'T) = Leaf value
 
-    static member createNode<'T> left right = HuffmanNode<_>.create left right
+    static member createNode<'T> (left: HuffmanTree<'T>) (right: HuffmanTree<'T>) = HuffmanNode<'T>.create left right
 
 type HuffmanNode<'T> = {
     Left: HuffmanTree<'T>
     Right: HuffmanTree<'T>
 } with
 
-    static member create<'T> left right = { Left = left; Right = right }
+    static member create<'T> (left: HuffmanTree<'T>) (right: HuffmanTree<'T>) = { Left = left; Right = right }
